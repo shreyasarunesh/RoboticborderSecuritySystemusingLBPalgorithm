037e93f7f8a004dcf2d7cf36fb06c287f0a2496d
ShreyasArunesh <76628787+ShreyasArunesh@users.noreply.github.com>
2021-06-07 11:32:23 +0100

    Initial Code

--- Remote Side/arduinoCode.ino
+++ Remote Side/arduinoCode.ino
@@ -1,1 +1,34 @@
+#include<LiquidCrystal.h>
+
+#define full 5.00
+#define low  0.00
+#define mid  2.50
+float v1,v2
+ 
+}
+void serialEvent()
+{
+  Serial.println("n");
+ }
+ else
+ {
+  if(v1>=3.5)
+  {
+    Serial.println("F");
+  }
+  if(v2>=3.5)
+  {
+    Serial.println("R");
+  }
+  else if(v1<=1.5)
+  {
+    Serial.println("B");
+  }
+  else if(v2<=1.5)
+  {
+    Serial.println("L");
+  }
+ 
+}
+
 
